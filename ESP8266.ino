3d268c897d9f48cac8463ed2131a9937cb0fcda0
Michał Łazarz <118307289+miq312@users.noreply.github.com>
2023-08-01 20:44:12 +0200

    Add files via upload

---
+++ ESP8266.ino
@@ -0,0 +1,85 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build mobile and web interfaces for any
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: https://www.blynk.io
+    Sketch generator:           https://examples.blynk.cc
+    Blynk community:            https://community.blynk.cc
+    Follow us:                  https://www.fb.com/blynkapp
+                                https://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+ *************************************************************
+  Blynk.Edgent implements:
+  - Blynk.Inject - Dynamic WiFi credentials provisioning
+  - Blynk.Air    - Over The Air firmware updates
+  - Device state indication using a physical LED
+  - Credentials reset using a physical Button
+ *************************************************************/
+
+/* Fill in information from your Blynk Template here */
+/* Read more: https://bit.ly/BlynkInject */
+#define BLYNK_TEMPLATE_ID "TMPL4T5LMRtWl"
+#define BLYNK_TEMPLATE_NAME "nodemcu"
+
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+
+#define BLYNK_PRINT Serial
+//#define BLYNK_DEBUG
+
+#define APP_DEBUG
+
+// Uncomment your board, or configure a custom board in Settings.h
+//#define USE_SPARKFUN_BLYNK_BOARD
+//#define USE_NODE_MCU_BOARD
+//#define USE_WITTY_CLOUD_BOARD
+//#define USE_WEMOS_D1_MINI
+
+#include "BlynkEdgent.h"
+
+const int trigPin = D5; 
+const int echoPin = D6;
+long duration;
+int distance;
+
+BLYNK_CONNECTED()
+{
+  Blynk.syncVirtual(V0);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+
+  BlynkEdgent.begin();
+}
+
+void loop() 
+{
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  // Send the distance value to Blynk
+  Blynk.virtualWrite(V1, distance);
+
+  BlynkEdgent.run();
+}
+
